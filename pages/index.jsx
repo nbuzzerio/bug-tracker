783af8b5ac024109ec38cdffeeda15a146ac2b0a
Nick Buzzerio <nick.buzzerio@hotmail.com>
2022-06-22 20:30:21 -0400

    added header, footer, dropdown navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,35 +1,40 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-
-  const [windowHeight, setWindowHeight] = useState(100)
-  const [windowWidth, setWindowWidth] = useState(100)
+  const [windowHeight, setWindowHeight] = useState(100);
+  const [windowWidth, setWindowWidth] = useState(100);
 
   useEffect(() => {
     const updateDimensions = () => {
-      setWindowWidth(window.innerWidth)
-      setWindowHeight(window.innerHeight)
-    }
-    updateDimensions()
-    window.addEventListener('resize', updateDimensions);
-    
-    return () => {
-    }
-  }, [])
-  
-  let numbersWidth = Array(Math.floor(windowWidth/15)).fill(1)
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    };
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
 
-  const numbersList = numbersWidth.map((column, index) => { 
-    let numbersHeight = Array(Math.floor(windowHeight/15)).fill(0).map((el) => Math.round(Math.random()*1))
-    let bg = 'bg-numGrad-'+(Math.floor(Math.random()*4)+1);
-    let delay = 'delay-'+(Math.floor(Math.random()*8));
+    return () => {};
+  }, []);
+
+  let numbersWidth = Array(Math.floor(windowWidth / 15)).fill(1);
+
+  const numbersList = numbersWidth.map((column, index) => {
+    let numbersHeight = Array(Math.floor(windowHeight / 15))
+      .fill(0)
+      .map((el) => Math.round(Math.random() * 1));
+    let bg = "bg-numGrad-" + (Math.floor(Math.random() * 4) + 1);
+    let delay = "delay-" + Math.floor(Math.random() * 8);
     return (
-      <div className={`binaryCol gradient-overlay ${delay} relative w-[1ch] flex flex-nowrap text-center justify-center p-2 text-[#fff]`} key={index} suppressHydrationWarning>
-        {numbersHeight.join(' ')}
+      <div
+        className={`binaryCol gradient-overlay ${delay} relative w-[1ch] flex flex-nowrap text-center justify-center p-2 text-[#fff]`}
+        key={index}
+        suppressHydrationWarning
+      >
+        {numbersHeight.join(" ")}
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div className="bg-black h-screen">
@@ -38,12 +43,12 @@ export default function Home() {
         <meta name="description" content="bug tracker" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <div className="bg-animation absolute w-full h-full top-0 left-0 overflow-hidden flex pointer-events-none">
         {numbersList}
       </div>
-
-      <main className="main absolute w-full h-full top-0 left-0">
-        <h1 className="text-7xl text-green-400 text-center">Bug Tracker</h1>
+      <main className="main absolute w-full h-full top-0 left-0 pt-[80px] lg:pt-[130px]">
+        <h1 className="text-7xl text-green-code text-center hidden">Bug Bytes</h1>
       </main>
     </div>
   );
